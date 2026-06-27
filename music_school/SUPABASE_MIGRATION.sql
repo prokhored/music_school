451eddb656eaf65a34bd186962af24ab5d8a73fd
prokhored <prokhored@gmail.com>
2026-06-28 03:32:16 +0500

    Delete music_school/SUPABASE_MIGRATION.sql

--- music_school/SUPABASE_MIGRATION.sql
+++
@@ -1,7 +0,0 @@
--- Добавление колонки account_id и связи с таблицей accounts
-alter table public.students
-add column if not exists account_id uuid references public.accounts(id) on delete cascade;
-
--- При необходимости сразу сделать account_id обязательным, если все строки привязаны к аккаунтам
--- alter table public.students
--- alter column account_id set not null;
